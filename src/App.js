de45e9427006c029b874baf1870c00e147611d2e
unknown <gausulislam5@gmail.com>
2023-09-30 20:20:36 +0600

    update props

--- src/App.js
+++ src/App.js
@@ -13,35 +13,36 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Tamim Iqbal" job="Batter"></Person>
+      <Person name="Mehedi Hasan Miraz" job="All Rounder"></Person>
+      <Person name="Musfiqur Rahim" job="Batter & wicket-kipper"></Person>
+      <Person name="Mahaduillah Riyad" job="All Rounder"></Person>
       <h5>New Components</h5>
       <p>React Mama Rock Mama</p>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
-      <Friend></Friend>
+      <Friend name="hemo" phone="0000"></Friend>
+      <Friend name="hemonto" phone="014555"></Friend>
+      <Friend name="Md Gausul" phone="01788"></Friend>
+      <Friend name="md Gausul Islam Hemonto" phone="01785003901"></Friend>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
+  // console.log(props);
   return (
     <div className="person">
-      <h1>Tamim Iqbal</h1>
-      <p>Profession: Cricketer</p>
+      <h1>{props.name}</h1>
+      <p>{props.job}</p>
     </div>
   )
 }
-function Friend(){
+function Friend(props){
+  console.log(props);
   return (
     <div className="container">
-      <h3>Name: Honey</h3>
-      <p>job: Misty</p>
+      <h3>{props.name}</h3>
+      <p>{props.phone}</p>
     </div>
   )
 }
-
 export default App;
