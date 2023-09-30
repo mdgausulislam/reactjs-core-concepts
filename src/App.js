afccf193317415721b875f22506f83fec7a45d7e
unknown <gausulislam5@gmail.com>
2023-09-30 19:41:24 +0600

    dynamic componente

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
-const number=5555;
-const singer={name:'Dr. Mahfuz',job:'singer'};
-const singer2={name:'Eva Rahman' ,job:'singer'};
+const number = 5555;
+const singer = { name: 'Dr. Mahfuz', job: 'singer' };
+const singer2 = { name: 'Eva Rahman', job: 'singer' };
 
-const singerStyle={
-  color:'purple',
-  backgroundColor:'white'
+const singerStyle = {
+  color: 'purple',
+  backgroundColor: 'white'
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello React How are you</h3>
-        </div>
-        <div className="music">
-          <p>Name: {2222+ number}</p>
-          <p style={singerStyle}>{singer.name} {singer.job}</p>
-          <p style={{color:'blue',backgroundColor:'yellow'}}>{singer2.name} {singer2.job}</p>
-        </div>
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New Components</h5>
+      <p>React Mama Rock Mama</p>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
     </div>
   );
+}
+
+function Person() {
+  return (
+    <div className="person">
+      <h1>Tamim Iqbal</h1>
+      <p>Profession: Cricketer</p>
+    </div>
+  )
+}
+function Friend(){
+  return (
+    <div className="container">
+      <h3>Name: Honey</h3>
+      <p>job: Misty</p>
+    </div>
+  )
 }
 
 export default App;
